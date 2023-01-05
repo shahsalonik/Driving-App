25889b03ff53b9a4dab4cb7f0d8bc3170e816d68
Prajakta Gadhamsetty <73443691+itsPrajaktaG@users.noreply.github.com>
2023-01-05 13:21:50 -0500

    Added timer in wrong section
    Implemented a timer in lines 25-31, however, i did this wrong, so imma change it once i figure out the right way lolz.

--- app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/MainActivity.java
+++ app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/MainActivity.java
@@ -1,26 +1,48 @@
 package com.shahsaloni_gadhamsettyprajakta.drivingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public int counter;
+    @TargetApi(Build.VERSION_CODES.O)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        final TextView counttime = findViewById(R.id.counttime);
+        new CountDownTimer(50000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                counttime.setText(String.valueOf(counter));
+                counter++;
+            }
 
-    public void startDriving(View view) {
+            @Override
+            public void onFinish() {
+                counttime.setText("Finished");
+            }
+        }.start();
+    }
+        public void startDriving(View view) {
         Intent intent = new Intent(getApplicationContext(), Driving.class);
         startActivity(intent);
-    }
+        }
 
-    public void viewLog(View view) {
+        public void viewLog(View view) {
         Intent intent = new Intent(getApplicationContext(), Log.class);
         startActivity(intent);
-    }
+        }
 }
