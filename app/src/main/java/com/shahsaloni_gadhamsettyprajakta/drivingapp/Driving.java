1a922df4338d0519f90abe9939488b9480cb4bf0
Saloni Shah <2023sshah@tjhsst.edu>
2023-01-30 23:32:25 -0500

    feat(driving): trying to implement a chronometer

--- app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/Driving.java
+++ app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/Driving.java
@@ -3,11 +3,18 @@ package com.shahsaloni_gadhamsettyprajakta.drivingapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Chronometer;
 
 import androidx.annotation.Nullable;
 
 public class Driving extends Activity {
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,5 +25,24 @@ public class Driving extends Activity {
     public void stopDriving(View view) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
     }
+
+    public void startChronometer(View view) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void pauseChronometer(View view) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
 }
--- app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/MainActivity.java
+++ app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.shahsaloni_gadhamsettyprajakta.drivingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
+
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
@@ -13,36 +16,53 @@ import android.widget.Toast;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
-    public int counter;
-    @TargetApi(Build.VERSION_CODES.O)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final TextView counttime = findViewById(R.id.counttime);
-        new CountDownTimer(50000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                counttime.setText(String.valueOf(counter));
-                counter++;
-            }
+        Chronometer chronometer;
+        ButtonBarLayout start, pause;
+        long pauseOffset;
+        boolean running;
 
-            @Override
-            public void onFinish() {
-                counttime.setText("Finished");
-            }
-        }.start();
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            chronometer = (Chronometer) findViewById(R.id.chronometer);
+            start = (ButtonBarLayout) findViewById(R.id.start_button);
+            pause = (ButtonBarLayout) findViewById(R.id.pause_button);
+
+            start.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                            if (!running) {
+                                    chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                                    chronometer.start();
+                                    running = true;
+                            }
+                    }
+            });
+
+            pause.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                            if (running) {
+                                    chronometer.stop();
+                                    pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                                    running = false;
+                            }
+                    }
+            });
+        }
+
         public void startDriving(View view) {
-        Intent intent = new Intent(getApplicationContext(), Driving.class);
-        startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), Driving.class);
+                startActivity(intent);
         }
 
         public void viewLog(View view) {
-        Intent intent = new Intent(getApplicationContext(), Log.class);
-        startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), Log.class);
+                startActivity(intent);
         }
 }
