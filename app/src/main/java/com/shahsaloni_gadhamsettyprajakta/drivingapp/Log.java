f71e60954a2939ad4b349c3fd4a76078ca764501
Saloni Shah <2023sshah@tjhsst.edu>
2023-01-31 15:44:27 -0500

    feat(export): working on exporting the log screen
    THIS DOESN'T WORK!! I'M STILL DEBUGGING!

--- app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/Log.java
+++ app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/Log.java
@@ -2,12 +2,22 @@ package com.shahsaloni_gadhamsettyprajakta.drivingapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
 
 import androidx.annotation.Nullable;
 
 public class Log extends Activity {
+
+    Button exportLog = findViewById(R.id.export_button);
+    RelativeLayout logScreen = findViewById(R.id.log_screen);
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +27,15 @@ public class Log extends Activity {
     public void back(View view) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
+    }
+
+    public void shareSheet (View view) {
+        Bitmap bitmap = Bitmap.createBitmap(logScreen.getWidth(), logScreen.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bitmap);
+        shareIntent.setType("image/jpeg");
+        startActivity(Intent.createChooser(shareIntent, null));
     }
 }
--- app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/MainActivity.java
+++ app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/MainActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ButtonBarLayout;
 
 import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
@@ -20,10 +25,20 @@ import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
 
+        Button exportLog;
+        RelativeLayout logScreen;
+        TableLayout tableLayout;
+
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+
+            exportLog = findViewById(R.id.export_button);
+            logScreen = findViewById(R.id.log_screen);
+            tableLayout = (TableLayout) findViewById(R.id.log_table);
+
         }
 
         public void startDriving(View view) {
