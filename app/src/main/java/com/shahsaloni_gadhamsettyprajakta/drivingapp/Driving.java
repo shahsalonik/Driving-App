fe36a04945c2e759b414301bf9eac743a3f4caa3
Prajakta Gadhamsetty <73443691+itsPrajaktaG@users.noreply.github.com>
2023-02-18 16:32:17 -0500

    map implementation gone wrong

--- app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/Driving.java
+++ app/src/main/java/com/shahsaloni_gadhamsettyprajakta/drivingapp/Driving.java
@@ -10,6 +10,10 @@ import android.widget.Chronometer;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -17,12 +21,14 @@ public class Driving extends Activity {
     private Chronometer chronometer;
     private long pauseOffset;
     public boolean running;
+    public MapView mapview;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driving);
-
+        mapview = findViewById(R.id.mapView);
+        mapview.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
         chronometer = findViewById(R.id.chronometer);
     }
 
